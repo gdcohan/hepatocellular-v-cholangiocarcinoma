36ea9e62fabb821b2e570065923b772491c38ffd
Robin <robinwang08@gmail.com>
2019-04-02 20:32:07 -0400

    Update calculate_features.py

--- calculate_features.py
+++ calculate_features.py
@@ -113,7 +113,8 @@ def run(folder, features_files, out, save=True):
     nrrd_features = nrrd_features.dropna()
 
     # nrrd_features = normalize_column(nrrd_features, column="age")
-    # nrrd_features = normalize_column(nrrd_features, column="volume")
+    nrrd_features = normalize_column(nrrd_features, column="volume")
+
     # patient, volume, outcome
     features_to_use = features(nrrd_features)
 
