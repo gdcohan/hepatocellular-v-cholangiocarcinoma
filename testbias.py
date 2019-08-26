ed21038783df497cc5c507e8dc8afdaa4eb45e2d
Robin <robinwang08@gmail.com>
2019-08-26 17:11:02 -0400

    Create testbias.py

---
+++ testbias.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+import SimpleITK as sitk
+import os
+from glob import glob
+import nibabel as nib
+import numpy as np
+
+def n4_bias_correction(image):
+    import ants
+    as_ants = ants.from_numpy(image)
+    corrected = ants.n4_bias_field_correction(as_ants)
+    return corrected.numpy()
+
+
+def stkbias(image, image_type=sitk.sitkFloat64):
+    input_image = sitk.ReadImage(image, image_type)
+    output_image = sitk.N4BiasFieldCorrection(input_image, input_image > 0)
+    return output_image
+
+
+patho = 'C:/Users/Robin/Downloads/test2'
+baseDir = os.path.normpath(patho)
+files = glob(baseDir + '/*/T1POST/*.nii')
+
+for file in files:
+    filePath, fileName = os.path.split(file)
+    a = filePath.split('\\')
+
+
+    startPath = 'C:/Users/Robin/Downloads/raw3/'
+    nePath = startPath + a[5]
+    neePath = nePath + '/T1POST'
+    newPath = neePath + '/' + fileName
+
+    if not os.path.isdir(startPath):
+        os.mkdir(startPath)
+
+    if not os.path.isdir(nePath):
+        os.mkdir(nePath)
+
+    if not os.path.isdir(neePath):
+        os.mkdir(neePath)
+
+    n1_img = nib.load(file)
+    n1_header = n1_img.header
+    n1_affine = n1_img.affine
+    img = n1_img.get_fdata()
+    img = n4_bias_correction(img)
+    imgmax = img.max()
+    img = np.true_divide(img, imgmax)
+    new_img = nib.Nifti1Image(img, n1_affine, n1_header)
+    nib.save(new_img, newPath)
